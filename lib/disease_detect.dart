c3c12047f8e5466b7bc63a16ec19eb9224a52b7f
Shweta Rajput <shwetakashyap942001@gmail.com>
2025-04-24 17:11:47 +0530

    Update disease_detect.dart

--- lib/disease_detect.dart
+++ lib/disease_detect.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'camera_screen.dart'; // Import the camera screen
-import 'home_page.dart'; // Import the Home Page
+import 'camera_screen.dart';
+import 'home_page.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:typed_data';
 import 'custom_app_bar.dart';
 import 'bottom_navigation_bar.dart';
-import 'prediction.dart'; // Import the prediction screen
+import 'prediction.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class CropDiseaseHome extends StatefulWidget {
   @override
@@ -15,16 +16,16 @@ class CropDiseaseHome extends StatefulWidget {
 }
 
 class _CropDiseaseHomeState extends State<CropDiseaseHome> {
-  Uint8List? _imageData; // To hold the selected image data
-  bool _isLoading = false; // Add loading state
-  int _currentIndex = 2; // Track the current index for bottom navigation
+  Uint8List? _imageData;
+  bool _isLoading = false;
+  int _currentIndex = 2;
 
   Future<void> _pickImage() async {
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text('Choose an option'),
+          title: Text('chooseOption'.tr()),
           actions: [
             TextButton(
               onPressed: () async {
@@ -37,12 +38,11 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
                   await _sendImage(_imageData!);
                 }
               },
-              child: Text('Upload from Gallery'),
+              child: Text('uploadFromGallery'.tr()),
             ),
             TextButton(
               onPressed: () async {
-                Navigator.of(context).pop(); // Close the dialog
-
+                Navigator.of(context).pop();
                 final imageBytes = await Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => CameraScreen()),
@@ -50,18 +50,18 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
 
                 if (imageBytes != null) {
                   setState(() {
-                    _imageData = imageBytes; // Set the captured image data
+                    _imageData = imageBytes;
                   });
-                  await _sendImage(_imageData!); // Send for disease detection
+                  await _sendImage(_imageData!);
                 }
               },
-              child: Text('Take a Picture'),
+              child: Text('takePicture'.tr()),
             ),
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('Cancel'),
+              child: Text('cancel'.tr()),
             ),
           ],
         );
@@ -71,7 +71,7 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
 
   Future<void> _sendImage(Uint8List imageData) async {
     setState(() {
-      _isLoading = true; // Set loading state to true
+      _isLoading = true;
     });
 
     try {
@@ -81,17 +81,15 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
       final response = await request.send();
       final responseData = await response.stream.toBytes();
       final result = String.fromCharCodes(responseData);
-
       final jsonResponse = jsonDecode(result);
 
       String prediction = jsonResponse['prediction'];
       String symptoms = (jsonResponse['symptoms'] as List).join('\n');
 
       setState(() {
-        _isLoading = false; // Set loading state to false after the response
+        _isLoading = false;
       });
 
-      // Navigate to the prediction screen after the prediction is ready
       Navigator.push(
         context,
         MaterialPageRoute(
@@ -104,10 +102,10 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
       );
     } catch (e) {
       setState(() {
-        _isLoading = false; // Set loading state to false in case of error
+        _isLoading = false;
       });
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error: $e')),
+        SnackBar(content: Text('errorOccurred'.tr())),
       );
     }
   }
@@ -115,14 +113,12 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
   void _onItemTapped(int index) {
     setState(() {
       _currentIndex = index;
-      // Navigate to different pages based on selected index
       if (_currentIndex == 0) {
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => HomePage()),
         );
       }
-      // You can add more navigation logic for other items if needed
     });
   }
 
@@ -138,7 +134,7 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
             Image.asset('assets/graphic_image.png', height: 200, width: 200),
             SizedBox(height: 20),
             Text(
-              'Health Check',
+              'healthCheck'.tr(),
               style: TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
@@ -147,7 +143,7 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
             ),
             SizedBox(height: 10),
             Text(
-              'Take a picture of your crop or upload an image to detect diseases and receive treatment advice.',
+              'healthCheckDescription'.tr(),
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 16, color: Color(0xFF222222)),
             ),
@@ -155,7 +151,7 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
             ElevatedButton.icon(
               onPressed: _pickImage,
               icon: Icon(Icons.upload),
-              label: Text('Upload Image & Detect'),
+              label: Text('uploadImageDetect'.tr()),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Color(0xFF3AAA49),
                 padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
@@ -164,12 +160,10 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
             ),
             SizedBox(height: 20),
             _imageData == null
-                ? Text('No image selected.', style: TextStyle(color: Color(0xFF222222)))
+                ? Text('noImageSelected'.tr(), style: TextStyle(color: Color(0xFF222222)))
                 : Column(
                     children: [
-                      // Image.memory(_imageData!, height: 200, width: 200), // Display captured or uploaded image
                       SizedBox(height: 20),
-                      // Show loading indicator while processing
                       if (_isLoading)
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -187,7 +181,7 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _pickImage, // Navigate to the image picker page
+        onPressed: _pickImage,
         child: Icon(Icons.camera_alt),
         backgroundColor: Color(0xFF088A6A),
       ),
@@ -198,7 +192,6 @@ class _CropDiseaseHomeState extends State<CropDiseaseHome> {
     );
   }
 
-  // Helper method to build a loading circle
   Widget _buildLoadingCircle(Color color) {
     return Container(
       width: 20,
